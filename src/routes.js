57470cf5ae1fddb2a21cdca2316fc468c5515b16
Letícia Barbosa Silva <leticiabarbosasilva94@gmail.com>
2019-11-09 08:34:56 -0300

    Instalation

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 import * as UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
 
 const router = new Router();
+const upload = multer(multerConfig);
 
 router.post('/sessions', SessionController.store);
 
 router.post('/users', UserController.store);
 router.put('/users/update/', authMiddleware, UserController.update);
+
+// Files
+router.post('/files/', authMiddleware, upload.single('file'), (req, res) => {
+  res.send('OK');
+});
 
 export default router;
---
+++ src/utils/randomChars.js
@@ -0,0 +1,3 @@
+export default () => {
+  return Math.floor(Math.random() * 10000 + 10000);
+};
